3eb38586cbf6d0d76efec64dece0c7a3f44c3733
Dev-Lucas-Silva <desenvolvedor.lucas.dev@gmail.com>
2023-11-26 22:32:48 -0300

    separacao de scripts em modulos

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
-import initCoverHeaderScroll from './modules/init-cover-header-scroll.js';
-import initScrollSuaveMenu from './modules/init-scroll-suave-menu.js';
-import initScrolltoTop from './modules/init-scroll-to-top.js';
-import SimpleAnime from './modules/simple-anime.js';
-
+import initCoverHeaderScroll from "./modules/init-cover-header-scroll.js";
+import initScrollSuaveMenu from "./modules/init-scroll-suave-menu.js";
+import initScrolltoTop from "./modules/init-scroll-to-top.js";
+import SimpleAnime from "./modules/simple-anime.js";
 
 initCoverHeaderScroll(".header");
 initScrollSuaveMenu(".header-menu a");
